d8959c07783195d2b1b0f6f927e628a252107ef1
arvidkahl <arvid@hey.com>
2024-05-01 10:05:28 -0400

    vis

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,7 @@
 <div class="container mx-auto px-4 sm:px-6 lg:px-8 mt-12">
     <div class="bg-white shadow overflow-hidden sm:rounded-lg">
         <div class="px-4 py-5 sm:px-6">
-            <h1 class="text-lg leading-6 font-medium text-gray-900">Most recently scanned podcast on Podscan.fm</h1>
+            <h1 class="text-lg leading-6 font-medium text-gray-900">🎙️ Most recently scanned podcast on Podscan.fm</h1>
         </div>
         @if($latestEpisode)
             <div class="border-t border-gray-200">
@@ -25,7 +25,7 @@
                     </div>
                     <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                         <dt class="text-sm font-medium text-gray-500">Description</dt>
-                        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 whitespace-pre-wrap">{!! $latestEpisode['episode_description'] !!}</dd>
+                        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 whitespace-pre-wrap [&_a]:underline">{!! $latestEpisode['episode_description'] !!}</dd>
                     </div>
                     <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                         <dt class="text-sm font-medium text-gray-500">Transcript</dt>
